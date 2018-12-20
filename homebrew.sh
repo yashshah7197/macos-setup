85afbe7d4c03b9938a2b1b3a3c67340367cea12b
yashshah7197 <yash.shah7197@live.com>
2018-12-20 21:07:34 +0530

    Add empty utils.sh and homebrew.sh scripts

---
+++ homebrew.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# homebrew.sh
+# This script contains functions for checking for and installing Homebrew,
+# tapping into Homebrew taps and installing Homebrew formulae and casks
---
+++ utils.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# utils.sh
+# This script contains some common utility functions which are used by all other scripts
