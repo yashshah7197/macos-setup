5d1bdff1a1f3443541bbee58b6b95adba76ac702
yashshah7197 <yash.shah7197@live.com>
2018-12-20 19:29:23 +0530

    Add functions to display info, failure & success messages

--- setup.sh
+++ setup.sh
@@ -2,3 +2,30 @@
 
 # setup.sh
 # This is the main script that will perform the macOS provisioning process
+
+# Declare different text colors and styles
+text_color_blue=$(tput setaf 4)
+text_color_green=$(tput setaf 2)
+text_color_red=$(tput setaf 1)
+text_color_white=$(tput setaf 7)
+text_style_bold=$(tput bold)
+text_style_default=$(tput sgr0)
+
+function newline() {
+    printf "\n"
+}
+
+function message_info() {
+    printf "${text_style_bold}${text_color_blue}==>${text_color_white} %s" "$1"
+    newline
+}
+
+function message_failure() {
+    printf "${text_style_bold}${text_color_red}✘${text_style_default}  %s" "$1"
+    newline
+}
+
+function message_success() {
+    printf "${text_style_bold}${text_color_green}✔${text_style_default}  %s" "$1"
+    newline
+}
