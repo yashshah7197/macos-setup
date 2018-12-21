6e98b6d9cda6f0acfcdc310cd42f536484a2f0f8
yashshah7197 <yash.shah7197@live.com>
2018-12-21 15:18:33 +0530

    Add function to install applications via the Mac App Store

--- apps.sh
+++ apps.sh
@@ -73,6 +73,19 @@ homebrew_casks=(
     'webpquicklook'
 )
 
+# List of macOS applications to be installed via the Mac App Store
+mas_apps=(
+    '1Password - 1333542190'
+    'Agenda - 1287445660'
+    'Blackmagic Disk Speed Test - 425264550'
+    'Keynote - 409183694'
+    'Slack - 803453959'
+    'Spark - 1176895641'
+    'WhatsApp - 1147396723'
+    'Wipr - 1320666476'
+    'Xcode - 497799835'
+)
+
 # Install command line tools and utilities via Homebrew
 function install_homebrew_formulae() {
     newline
@@ -115,5 +128,29 @@ function install_homebrew_casks() {
     message_success "Successfully installed $cask_count_success applications via Homebrew cask!"
     if [ $cask_count_failure -ne 0 ]; then
         message_failure "Failed to install $cask_count_failure applications via Homebrew cask!"
+    fi
+}
+
+# Install macOS applications via the Mac App Store
+function install_mas_apps() {
+    newline
+    message_info "Installing applications via the Mac App Store..."
+    mas_count_success=0
+    mas_count_failure=0
+    for app in "${mas_apps[@]}"; do
+        app_name=$(awk -F- '{print $1}' <<< $app | awk '{$1=$1};1')
+        app_id=$(awk -F- '{print $2}' <<< $app | awk '{$1=$1};1')
+        printf "${text_style_default}Installing $app_name..."
+        if mas install $app_id >/dev/null 2>&1; then
+            printf "${text_style_bold}${text_color_green}✔${text_style_default}\n"
+            ((++mas_count_success))
+        else
+            printf "${text_style_bold}${text_color_red}✘${text_style_default}\n"
+            ((++mas_count_failure))
+        fi
+    done
+    message_success "Successfully installed $mas_count_success applications via the Mac App Store!"
+    if [ $mas_count_failure -ne 0 ]; then
+        message_failure "Failed to install $mas_count_failure applications via the Mac App Store!"
     fi
 }
