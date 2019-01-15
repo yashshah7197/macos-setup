ee8a0f8fbe82bb0bd54de93d8c0a8a8e97e4aa8f
yashshah7197 <yash.shah7197@live.com>
2019-01-15 11:47:43 +0530

    Reformat fonts.sh

--- fonts.sh
+++ fonts.sh
@@ -5,8 +5,8 @@
 
 # List of fonts to be installed via Homebrew cask
 homebrew_fonts=(
-    'font-fira-code'
-    'font-roboto'
+    'Fira Code -- font-fira-code'
+    'Roboto -- font-roboto'
 )
 
 # Install fonts via Homebrew cask
@@ -14,8 +14,10 @@ function install_homebrew_fonts() {
     newline
     message_info "Installing fonts via Homebrew cask..."
     for font in "${homebrew_fonts[@]}"; do
-        printf "${text_style_default}Installing $font..."
-        if brew cask install $font >/dev/null 2>&1; then
+        font_name=$(awk -F-- '{print $1}' <<< "${font}" | awk '{$1=$1};1')
+        font_id=$(awk -F-- '{print $2}' <<< "${font}" | awk '{$1=$1};1')
+        printf "${text_style_default}Installing ${font_name}..."
+        if brew cask install "${font_id}" >/dev/null 2>&1; then
             print_tick
         else
             print_cross
