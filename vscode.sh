356d8868ffbc5491e5409dd3bca372626b947aa8
yashshah7197 <yash@yashshah.io>
2020-11-19 01:26:59 +0530

    Tweak VS Code setup

--- vscode.sh
+++ vscode.sh
@@ -9,42 +9,37 @@ readonly VSCODE_DOTFILES_SETTINGS_DIR="${DOTFILES_DIR}"/vscode
 
 # List of Visual Studio Code extensions to be installed
 vscode_extensions=(
-    'Bracket Pair Colorizer 2 -- coenraads.bracket-pair-colorizer-2'
-    'VS Code Icons -- vscode-icons-team.vscode-icons'
+    'coenraads.bracket-pair-colorizer-2'
+    'vscode-icons-team.vscode-icons'
+    'skyapps.fish-vscode'
 )
 
 # Install Visual Studio Code extensions
 function vscode_install_extensions() {
-    newline
-    message_info "Installing Visual Studio Code extensions..."
+    message_normal "Installing extensions..."
     for extension in "${vscode_extensions[@]}"; do
-        extension_name=$(awk -F-- '{print $1}' <<< "${extension}" | awk '{$1=$1};1')
-        extension_identifier=$(awk -F-- '{print $2}' <<< "${extension}" | awk '{$1=$1};1')
-        message_normal "Installing ${extension_name}..."
-        if code --install-extension "${extension_identifier}" \
+        if ! code --install-extension "${extension}" \
             >/dev/null 2>"${FILENAME_LOG_ERRORS}"; then
-            print_tick
-        else
             print_cross
             newline
             print_error_and_exit
         fi
     done
-    message_success "Successfully installed Visual Studio Code extensions!"
+    print_tick
 }
 
 # Create a symlink for the Visual Studio Code settings file
 function vscode_symlink_settings() {
-    message_normal "Symlinking the Visual Studio Code settings file..."
+    message_normal "Symlinking settings.json..."
     ln -f -s "${VSCODE_DOTFILES_SETTINGS_DIR}"/settings.json "${VSCODE_SETTINGS_DIR}"/settings.json
     print_tick
 }
 
 # Main function to kick-off setting up Visual Studio Code
 function setup_vscode() {
-    vscode_install_extensions
     newline
     message_info "Setting up Visual Studio Code..."
+    vscode_install_extensions
     vscode_symlink_settings
     message_success "Successfully set up Visual Studio Code!"
 }
